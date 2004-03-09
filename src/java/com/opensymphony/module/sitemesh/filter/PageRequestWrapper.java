89f581e68f1acab6093bbd739b399bfbd29d7763
mbogaert <mbogaert>
2004-03-09 15:48:10 +0000

    Removed IDEA cruft.

--- src/java/com/opensymphony/module/sitemesh/filter/PageRequestWrapper.java
+++ src/java/com/opensymphony/module/sitemesh/filter/PageRequestWrapper.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequestWrapper;
  * Will wrap a request for the {@link RequestDispatcherWrapper}.
  *
  * @author <a href="mailto:joeo@enigmastation.com">Joseph B. Ottinger</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class PageRequestWrapper extends HttpServletRequestWrapper {
     boolean debug = false;
@@ -23,6 +23,6 @@ public class PageRequestWrapper extends HttpServletRequestWrapper {
     }
 
     public RequestDispatcher getRequestDispatcher(String s) {
-        return new RequestDispatcherWrapper(super.getRequestDispatcher(s));    //To change body of overridden methods use File | Settings | File Templates.
+        return new RequestDispatcherWrapper(super.getRequestDispatcher(s));
     }
 }
