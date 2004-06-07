9b147305df7e713fe736047a1131b90cecb9b17c
Joe Walnes <joe@walnes.com>
2004-06-07 09:38:51 +0000

    Added tests to ensure Content-length header matches actual content length.

--- testsuite/src/java/testsuite/sitemesh/ContentLengthTest.java
+++ testsuite/src/java/testsuite/sitemesh/ContentLengthTest.java
@@ -15,4 +15,18 @@ public class ContentLengthTest extends WebTest {
         assertTrue("Document is not complete: " + rs.getText(), rs.getText().trim().endsWith("</html>"));
     }
 
+    public void testContentMatchesContentLengthUsingWriter() throws Exception {
+        WebResponse rs = wc.getResponse(server.getBaseURL() + "/outputservlet?out=writer");
+        if (rs.getHeaderField("Content-length") != null) {
+            assertEquals(rs.getHeaderField("Content-length"), String.valueOf(rs.getText().getBytes().length));
+        }
+    }
+
+    public void testContentMatchesContentLengthUsingStream() throws Exception {
+        WebResponse rs = wc.getResponse(server.getBaseURL() + "/outputservlet?out=stream");
+        if (rs.getHeaderField("Content-length") != null) {
+            assertEquals(rs.getHeaderField("Content-length"), String.valueOf(rs.getText().getBytes().length));
+        }
+    }
+
 }
