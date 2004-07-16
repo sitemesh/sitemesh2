e689836731efc227a0f305189da638e815890b52
farkas <farkas>
2004-07-16 06:25:43 +0000

    Issue number: SIM-54 Cleaned up Content Length Test

--- testsuite/src/java/testsuite/sitemesh/ContentLengthTest.java
+++ testsuite/src/java/testsuite/sitemesh/ContentLengthTest.java
@@ -3,11 +3,12 @@ package testsuite.sitemesh;
 import com.meterware.httpunit.WebResponse;
 import testsuite.tester.WebTest;
 
+import java.io.IOException;
+
+import org.xml.sax.SAXException;
+
 public class ContentLengthTest extends WebTest {
 
-    /**
-     * This seems to fail in Weblogic 7.0 SP4.  I have debugged the code, and can't see what the problem is. - SF 15/July/04
-     */
     public void testCompleteContentFromUndecoratedPageUsingWriter() throws Exception {
         WebResponse rs = wc.getResponse(server.getBaseURL() + "/outputservlet?out=writer");
         assertTrue("Document is not complete: " + rs.getText(), rs.getText().trim().endsWith("</html>"));
@@ -19,16 +20,20 @@ public class ContentLengthTest extends WebTest {
     }
 
     public void testContentMatchesContentLengthUsingWriter() throws Exception {
-        WebResponse rs = wc.getResponse(server.getBaseURL() + "/outputservlet?out=writer");
-        if (rs.getHeaderField("Content-length") != null) {
-            assertEquals(rs.getHeaderField("Content-length"), String.valueOf(rs.getText().getBytes().length));
-        }
+        _testContentLength("/outputservlet?out=writer");
     }
 
     public void testContentMatchesContentLengthUsingStream() throws Exception {
-        WebResponse rs = wc.getResponse(server.getBaseURL() + "/outputservlet?out=stream");
-        if (rs.getHeaderField("Content-length") != null) {
-            assertEquals(rs.getHeaderField("Content-length"), String.valueOf(rs.getText().getBytes().length));
+        _testContentLength("/outputservlet?out=stream");
+    }
+
+     private void _testContentLength(String url) throws IOException, SAXException {
+        WebResponse rs = wc.getResponse(server.getBaseURL() + url);
+        final String contentLengthHeader = rs.getHeaderField("Content-length");
+        if (contentLengthHeader != null) {
+            final String contentLength = String.valueOf(rs.getText().getBytes().length);
+            assertEquals("Content Length Header was '" + contentLengthHeader + "' but content length was '" + contentLength + "'",
+                    contentLengthHeader, contentLength);
         }
     }
 
