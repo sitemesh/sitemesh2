9a64276b369638231562b21766cf80732a641d0e
farkas <farkas>
2004-07-16 06:32:02 +0000

    Issue number:  SIM-114 Fix problems with line endings not making it correctly through.  This was because the println() method was not being overridden. This fixes content length problems on all platforms as far as I can see.

--- src/java/com/opensymphony/module/sitemesh/filter/PageWriter.java
+++ src/java/com/opensymphony/module/sitemesh/filter/PageWriter.java
@@ -10,109 +10,153 @@
 package com.opensymphony.module.sitemesh.filter;
 
 import java.io.CharArrayWriter;
-import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.Writer;
 
 /**
  * Implementation of PrintWriter that stores all data written
  * to it in a temporary buffer accessible from {@link #getBuffer(java.lang.String)} .
  *
  * @author <a href="joe@truemesh.com">Joe Walnes</a>
- * @version $Revision: 1.2 $
+ * @author <a href="scott@atlassian.com">Scott Farquhar</a>
+ * @version $Revision: 1.3 $
  */
 public class PageWriter extends PrintWriter implements OutputBuffer {
     private CharArrayWriter buffer = null;
-    private Writer original = null;
-    private Writer target = null;
+    private PrintWriter bufferWiter = null;
+    private PrintWriter original = null;
+    private PrintWriter target = null;
 
-    public PageWriter(Writer original) throws IOException {
+    public PageWriter(PrintWriter original) {
         super(original);
         this.original = original;
         buffer = new CharArrayWriter(1000);
-        target = buffer;
+        bufferWiter = new PrintWriter(buffer);
+        target = bufferWiter;
     }
 
     public void write(char buf[]) {
-        try {
-            target.write(buf);
-        }
-        catch (IOException e) {
-            setError();
-        }
+        target.write(buf);
     }
 
     public void write(String s, int off, int len) {
-        try {
-            target.write(s, off, len);
-        }
-        catch (IOException e) {
-            setError();
-        }
+        target.write(s, off, len);
     }
 
     public void write(String s) {
-        try {
-            target.write(s);
-        }
-        catch (IOException e) {
-            setError();
-        }
+        target.write(s);
     }
 
-
     public void write(char[] c, int o, int l) {
-        try {
-            target.write(c, o, l);
-        }
-        catch (IOException e) {
-            setError();
-        }
+        target.write(c, o, l);
     }
 
     public void write(int i) {
-        try {
-            target.write(i);
-        }
-        catch (IOException e) {
-            setError();
-        }
+        target.write(i);
     }
 
     public void flush() {
-        try {
-            target.flush();
-        }
-        catch (IOException e) {
-            setError();
-        }
+        target.flush();
     }
 
     public void close() {
-        try {
-            target.close();
-        }
-        catch (IOException e) {
-            setError();
-        }
+        target.close();
     }
 
-    /** Return all data that has been written to this Writer. */
+    public void println() {
+        target.println();
+    }
+
+    public boolean checkError() {
+        return target.checkError();
+    }
+
+    public void print(char c) {
+        target.print(c);
+    }
+
+    public void println(char x) {
+        target.println(x);
+    }
+
+    public void print(double d) {
+        target.print(d);
+    }
+
+    public void println(double x) {
+        target.println(x);
+    }
+
+    public void print(float f) {
+        target.print(f);
+    }
+
+    public void println(float x) {
+        target.println(x);
+    }
+
+    public void print(int i) {
+        target.print(i);
+    }
+
+    public void println(int x) {
+        target.println(x);
+    }
+
+    public void print(long l) {
+        target.print(l);
+    }
+
+    public void println(long x) {
+        target.println(x);
+    }
+
+    public void print(boolean b) {
+        target.print(b);
+    }
+
+    public void println(boolean x) {
+        target.println(x);
+    }
+
+    public void print(char s[]) {
+        target.print(s);
+    }
+
+    public void println(char x[]) {
+        target.println(x);
+    }
+
+    public void print(Object obj) {
+        target.print(obj);
+    }
+
+    public void println(Object x) {
+        target.println(x);
+    }
+
+    public void print(String s) {
+        target.print(s);
+    }
+
+    public void println(String x) {
+        target.println(x);
+    }
+
+    /**
+     * Return all data that has been written to this Writer.
+     */
     public char[] getBuffer(String encoding) {
         return buffer.toCharArray();
     }
 
     public void discardBuffer() {
         //allow multiple calls to this method without duplicating content to target
-        if (target == buffer) {
+        if (target == bufferWiter) {
             target = original;
-            try {
-                // copy any bytes written to buffer so far to original stream.
-                original.write(buffer.toCharArray());
-                original.flush();
-            } catch (IOException e) {
-                e.printStackTrace(); // this should never happen.
-            }
+
+            // copy any bytes written to buffer so far to original stream.
+            original.write(buffer.toCharArray());
+            original.flush();
         }
     }
 }
