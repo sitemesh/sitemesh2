077fa38a979cf67cd732707c109d19fdaacb167e
Joe Walnes <joe@walnes.com>
2004-08-19 07:12:44 +0000

    Upload snapshot jar

--- continuous-integration/build.sh
+++ continuous-integration/build.sh
@@ -3,4 +3,6 @@
 (cd testsuite; ant clean)            # clean everything, main sitemesh and acceptance tests
 ant test                             # unit tests
 (cd testsuite; ant test-embedded)    # acceptance tests
+ant -Dversion=SNAPSHOT jar           # build snapshot jar
 
+scp dist/sitemesh-SNAPSHOT.jar beaver:/home/users/joe/public_html # upload snapshot (obviously this is a temp location)
