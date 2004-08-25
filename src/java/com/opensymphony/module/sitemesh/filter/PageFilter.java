826b027f1e9dd3885543ef5abf7b535f307a61df
hani <hani>
2004-08-25 15:25:45 +0000

    I'm an idiot

--- src/java/com/opensymphony/module/sitemesh/filter/PageFilter.java
+++ src/java/com/opensymphony/module/sitemesh/filter/PageFilter.java
@@ -23,7 +23,7 @@ import java.io.PrintWriter;
  *
  * @author <a href="joe@truemesh.com">Joe Walnes</a>
  * @author <a href="scott@atlassian.com">Scott Farquhar</a>
- * @version $Revision: 1.9 $
+ * @version $Revision: 1.10 $
  */
 public class PageFilter implements Filter, RequestConstants {
     private FilterConfig filterConfig = null;
@@ -140,7 +140,7 @@ public class PageFilter implements Filter, RequestConstants {
                 // parse the page
                 result = pageResponse.getPage();
             }
-            request.setAttribute(USING_STREAM, Boolean.valueOf(pageResponse.isUsingStream()));
+            request.setAttribute(USING_STREAM, new Boolean(pageResponse.isUsingStream()));
             pageResponse.closeWriter();
             return result;
         }
