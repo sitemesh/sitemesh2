11a00e8532338952875a04854541b4b7ac490c1f
Joe Walnes <joe@walnes.com>
2004-10-02 14:45:20 +0000

    Noise

--- src/test/com/opensymphony/module/sitemesh/mapper/ConfigLoaderTest.java
+++ src/test/com/opensymphony/module/sitemesh/mapper/ConfigLoaderTest.java
@@ -1,7 +1,6 @@
 package com.opensymphony.module.sitemesh.mapper;
 
 import junit.framework.TestCase;
-import junit.framework.Assert;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -74,50 +73,50 @@ public class ConfigLoaderTest extends TestCase {
     }
 
     public void testMappedNames() throws Exception {
-        Assert.assertEquals(configLoader.getMappedName("/info/somepage.html"), "default");
-        Assert.assertEquals(configLoader.getMappedName("/test/somepage.html"), "default");
+        assertEquals(configLoader.getMappedName("/info/somepage.html"), "default");
+        assertEquals(configLoader.getMappedName("/test/somepage.html"), "default");
 
-        Assert.assertEquals(configLoader.getMappedName("/other/someotherpage.html"), "other");
+        assertEquals(configLoader.getMappedName("/other/someotherpage.html"), "other");
 
-        Assert.assertEquals(configLoader.getMappedName("/uri/somepage.html"), "uri");
+        assertEquals(configLoader.getMappedName("/uri/somepage.html"), "uri");
 
-        Assert.assertEquals(configLoader.getMappedName("/rolebased/someotherpage.html"), "rolebaseddeveloper");
+        assertEquals(configLoader.getMappedName("/rolebased/someotherpage.html"), "rolebaseddeveloper");
 
-        Assert.assertEquals(configLoader.getMappedName("/old/someoldpage.html"), "old");
+        assertEquals(configLoader.getMappedName("/old/someoldpage.html"), "old");
     }
 
     public void testDecoratorPresence() throws Exception {
-        Assert.assertNotNull(configLoader.getDecoratorByName("default"));
-        Assert.assertNotNull(configLoader.getDecoratorByName("other"));
-        Assert.assertNotNull(configLoader.getDecoratorByName("uri"));
-        Assert.assertNotNull(configLoader.getDecoratorByName("rolebaseddeveloper"));
-        Assert.assertNotNull(configLoader.getDecoratorByName("old"));
+        assertNotNull(configLoader.getDecoratorByName("default"));
+        assertNotNull(configLoader.getDecoratorByName("other"));
+        assertNotNull(configLoader.getDecoratorByName("uri"));
+        assertNotNull(configLoader.getDecoratorByName("rolebaseddeveloper"));
+        assertNotNull(configLoader.getDecoratorByName("old"));
     }
 
     public void testDecorators() throws Exception {
-        Assert.assertEquals(configLoader.getDecoratorByName("default").getName(), "default");
-        Assert.assertEquals(configLoader.getDecoratorByName("default").getPage(), "/decorators/default.jsp");
-        Assert.assertNull(configLoader.getDecoratorByName("default").getURIPath());
-        Assert.assertNull(configLoader.getDecoratorByName("default").getRole());
+        assertEquals(configLoader.getDecoratorByName("default").getName(), "default");
+        assertEquals(configLoader.getDecoratorByName("default").getPage(), "/decorators/default.jsp");
+        assertNull(configLoader.getDecoratorByName("default").getURIPath());
+        assertNull(configLoader.getDecoratorByName("default").getRole());
 
-        Assert.assertEquals(configLoader.getDecoratorByName("other").getName(), "other");
-        Assert.assertEquals(configLoader.getDecoratorByName("other").getPage(), "/decorators/other.jsp");
-        Assert.assertNull(configLoader.getDecoratorByName("other").getURIPath());
-        Assert.assertNull(configLoader.getDecoratorByName("other").getRole());
+        assertEquals(configLoader.getDecoratorByName("other").getName(), "other");
+        assertEquals(configLoader.getDecoratorByName("other").getPage(), "/decorators/other.jsp");
+        assertNull(configLoader.getDecoratorByName("other").getURIPath());
+        assertNull(configLoader.getDecoratorByName("other").getRole());
 
-        Assert.assertEquals(configLoader.getDecoratorByName("uri").getName(), "uri");
-        Assert.assertEquals(configLoader.getDecoratorByName("uri").getPage(), "/decorators/uri.jsp");
-        Assert.assertEquals(configLoader.getDecoratorByName("uri").getURIPath(), "/someapp");
-        Assert.assertNull(configLoader.getDecoratorByName("uri").getRole());
+        assertEquals(configLoader.getDecoratorByName("uri").getName(), "uri");
+        assertEquals(configLoader.getDecoratorByName("uri").getPage(), "/decorators/uri.jsp");
+        assertEquals(configLoader.getDecoratorByName("uri").getURIPath(), "/someapp");
+        assertNull(configLoader.getDecoratorByName("uri").getRole());
 
-        Assert.assertEquals(configLoader.getDecoratorByName("rolebaseddeveloper").getName(), "rolebased");
-        Assert.assertEquals(configLoader.getDecoratorByName("rolebaseddeveloper").getPage(), "/decorators/rolebased.jsp");
-        Assert.assertNull(configLoader.getDecoratorByName("rolebaseddeveloper").getURIPath());
-        Assert.assertEquals(configLoader.getDecoratorByName("rolebaseddeveloper").getRole(), "developer");
+        assertEquals(configLoader.getDecoratorByName("rolebaseddeveloper").getName(), "rolebased");
+        assertEquals(configLoader.getDecoratorByName("rolebaseddeveloper").getPage(), "/decorators/rolebased.jsp");
+        assertNull(configLoader.getDecoratorByName("rolebaseddeveloper").getURIPath());
+        assertEquals(configLoader.getDecoratorByName("rolebaseddeveloper").getRole(), "developer");
 
-        Assert.assertEquals(configLoader.getDecoratorByName("old").getName(), "old");
-        Assert.assertEquals(configLoader.getDecoratorByName("old").getPage(), "/decorators/old.jsp");
-        Assert.assertNull(configLoader.getDecoratorByName("old").getURIPath());
-        Assert.assertNull(configLoader.getDecoratorByName("old").getRole());
+        assertEquals(configLoader.getDecoratorByName("old").getName(), "old");
+        assertEquals(configLoader.getDecoratorByName("old").getPage(), "/decorators/old.jsp");
+        assertNull(configLoader.getDecoratorByName("old").getURIPath());
+        assertNull(configLoader.getDecoratorByName("old").getRole());
     }
 }
