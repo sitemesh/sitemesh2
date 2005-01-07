2eda67851f2121e61c1989e903af49e57c60a2b3
Joe Walnes <joe@walnes.com>
2005-01-07 15:42:46 +0000

    Use cp instead of scp

--- continuous-integration/build.sh
+++ continuous-integration/build.sh
@@ -5,4 +5,5 @@ ant test                             # unit tests
 (cd testsuite; ant test-embedded)    # acceptance tests
 ant -Dversion=SNAPSHOT jar           # build snapshot jar
 
-scp dist/sitemesh-SNAPSHOT.jar beaver:/home/users/joe/public_html # upload snapshot (obviously this is a temp location)
+#scp dist/sitemesh-SNAPSHOT.jar beaver:/home/users/joe/public_html # upload snapshot (obviously this is a temp location)
+cp dist/sitemesh-SNAPSHOT.jar /home/users/joe/public_html
