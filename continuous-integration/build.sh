d0faa4a696f245995ef85bba1ce8082e35e599ca
Joe Walnes <joe@walnes.com>
2005-01-29 11:41:04 +0000

    Replaced continuous integration shell script with ant build file (so it's easier to run on different platforms)

--- continuous-integration/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-(cd testsuite; ant clean)            # clean everything, main sitemesh and acceptance tests
-ant test                             # unit tests
-(cd testsuite; ant test-embedded)    # acceptance tests
-ant -Dversion=SNAPSHOT jar           # build snapshot jar
-
-#scp dist/sitemesh-SNAPSHOT.jar beaver:/home/users/joe/public_html # upload snapshot (obviously this is a temp location)
-cp dist/sitemesh-SNAPSHOT.jar /home/users/joe/public_html
