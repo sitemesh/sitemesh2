1c4ab22fa3da503381bb81a911261396c5168ee9
Scott Murphy <scott@alwaysvip.com>
2023-07-13 18:50:38 -0700

    Remove redundant NoDecorartor flush

--- src/java/com/opensymphony/sitemesh/webapp/decorator/NoDecorator.java
+++ src/java/com/opensymphony/sitemesh/webapp/decorator/NoDecorator.java
@@ -21,19 +21,15 @@ public class NoDecorator extends BaseWebAppDecorator {
     protected void render(Content content, HttpServletRequest request, HttpServletResponse response,
                           ServletContext servletContext, SiteMeshWebAppContext webAppContext)
             throws IOException, ServletException {
-
-        response.setContentLength(content.originalLength());
-
+        
+        PrintWriter writer;
         if (webAppContext.isUsingStream()) {
-            PrintWriter writer = new PrintWriter(response.getOutputStream());
-            content.writeOriginal(writer);
-            writer.flush(); //flush writer to underlying outputStream
-            response.getOutputStream().flush();
+            writer = new PrintWriter(response.getOutputStream());
         } else {
-            PrintWriter writer = response.getWriter();
-            content.writeOriginal(writer);
-            response.getWriter().flush();
+            writer = response.getWriter();
         }
+        content.writeOriginal(writer);
+        writer.flush();
     }
 
 }
