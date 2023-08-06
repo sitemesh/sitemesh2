bccdc097c614181b17c0b7268d9ab60d6ed009af
Scott Murphy <scott@alwaysvip.com>
2023-08-06 14:11:51 -0600

    Fix mockito unit test

--- src/test/com/opensymphony/module/sitemesh/scalability/MockHttpServletRequest.java
+++ src/test/com/opensymphony/module/sitemesh/scalability/MockHttpServletRequest.java
@@ -1,10 +1,8 @@
 package com.opensymphony.module.sitemesh.scalability;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletInputStream;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
@@ -227,6 +225,11 @@ public class MockHttpServletRequest implements HttpServletRequest
         return httpSession;
     }
 
+    @Override
+    public String changeSessionId() {
+        return null;
+    }
+
     public boolean isRequestedSessionIdValid()
     {
         return false;
@@ -245,6 +248,36 @@ public class MockHttpServletRequest implements HttpServletRequest
     public boolean isRequestedSessionIdFromUrl()
     {
         return false;
+    }
+
+    @Override
+    public boolean authenticate(HttpServletResponse response) throws IOException, ServletException {
+        return false;
+    }
+
+    @Override
+    public void login(String username, String password) throws ServletException {
+
+    }
+
+    @Override
+    public void logout() throws ServletException {
+
+    }
+
+    @Override
+    public Collection<Part> getParts() throws IOException, ServletException {
+        return null;
+    }
+
+    @Override
+    public Part getPart(String name) throws IOException, ServletException {
+        return null;
+    }
+
+    @Override
+    public <T extends HttpUpgradeHandler> T upgrade(Class<T> handlerClass) throws IOException, ServletException {
+        return null;
     }
 
     public Object getAttribute(String key)
@@ -270,6 +303,11 @@ public class MockHttpServletRequest implements HttpServletRequest
 
     public int getContentLength()
     {
+        return 0;
+    }
+
+    @Override
+    public long getContentLengthLong() {
         return 0;
     }
 
@@ -421,6 +459,41 @@ public class MockHttpServletRequest implements HttpServletRequest
     public int getLocalPort()
     {
         return 0;
+    }
+
+    @Override
+    public ServletContext getServletContext() {
+        return null;
+    }
+
+    @Override
+    public AsyncContext startAsync() throws IllegalStateException {
+        return null;
+    }
+
+    @Override
+    public AsyncContext startAsync(ServletRequest servletRequest, ServletResponse servletResponse) throws IllegalStateException {
+        return null;
+    }
+
+    @Override
+    public boolean isAsyncStarted() {
+        return false;
+    }
+
+    @Override
+    public boolean isAsyncSupported() {
+        return false;
+    }
+
+    @Override
+    public AsyncContext getAsyncContext() {
+        return null;
+    }
+
+    @Override
+    public DispatcherType getDispatcherType() {
+        return null;
     }
 
     public void setRequestURL(String requestURL)
